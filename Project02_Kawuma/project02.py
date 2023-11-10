99885e75a1003e505abad07211b3123d80347c04
skawuma <82976186+skawuma@users.noreply.github.com>
2023-11-09 19:08:59 -0500

    changes made

--- Project02_Kawuma/project02.py
+++ Project02_Kawuma/project02.py
@@ -3,13 +3,16 @@ def evenly_divides():
         try:
             num1 = int(input("Enter the first positive integer: "))
             num2 = int(input("Enter the second positive integer: "))
+            # condition to check If both numbers are positive integers,
             if num1 <= 0 or num2 <= 0:
                 print("Both numbers must be positive integers. Please try again.")
                 continue
+            # Condition to check if the function checks whether either one evenly divides the other 
             if num1 % num2 == 0 or num2 % num1 == 0:
                 return True
             else:
                 return False
+            #try and except block to get an invalid input is detected, an error message is displayed
         except ValueError:
             print("Invalid input. Please enter positive integers.")
 
